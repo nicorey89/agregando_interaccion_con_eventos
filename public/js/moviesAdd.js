d180115d1f2f43763d5463397b6f95c9294d80f9
nicorey89 <nicolasthian8907@gmail.com>
2023-06-18 00:23:40 -0300

    terminando TP

--- public/js/moviesAdd.js
+++ public/js/moviesAdd.js
@@ -1,12 +1,38 @@
-window.onload = function(){
-    let titulo = document.querySelector('.moviesAddTitulo')
-    let formulario = document.querySelector('#formulario');
-    let article = document.querySelector('article');
-    titulo.innerHTML = 'AGREGAR PELÍCULA';
-    titulo.classList.add('titulo');
-    article.classList.add('fondoTransparente');
-    formulario.classList.add('fondoCRUD');
-
-
-
-}
+window.onload = function () {
+    let titulo = document.querySelector(".moviesAddTitulo");
+    let formulario = document.querySelector("#formulario");
+    let article = document.querySelector("article");
+    titulo.innerHTML = "AGREGAR PELÍCULA";
+    titulo.classList.add("titulo");
+    article.classList.add("fondoTransparente");
+    formulario.classList.add("fondoCRUD");
+    let input = document.querySelector("#titulo");
+    let estadoSecreto = 0;
+    input.addEventListener("keyup", (e) => {
+          switch (estadoSecreto) {
+                case 0:
+                      e.key == "s" ? (estadoSecreto = 1) : (estadoSecreto = 0);
+                      break;
+                case 1:
+                      e.key == "e" ? (estadoSecreto = 2) : (estadoSecreto = 0);
+                      break;
+                case 2:
+                      e.key == "c" ? (estadoSecreto = 3) : (estadoSecreto = 0);
+                      break;
+                case 3:
+                      e.key == "r" ? (estadoSecreto = 4) : (estadoSecreto = 0);
+                      break;
+                case 4:
+                      e.key == "e" ? (estadoSecreto = 5) : (estadoSecreto = 0);
+                      break;
+                case 5:
+                      e.key == "t" ? (estadoSecreto = 6) : (estadoSecreto = 0);
+                      break;
+                case 6:
+                      e.key == "o" ? alert("SECRETO MAGICO") : (estadoSecreto = 0);
+                      break;
+                default:
+                      break;
+          }
+    });
+};
--- public/js/moviesList.js
+++ public/js/moviesList.js
@@ -1,17 +1,30 @@
-window.onload = function(){
-    let body = document.querySelector('body');
-    let moviesListTitulo = document.querySelector('.moviesListTitulo');
+window.onload = function () {
+    let body = document.querySelector("body");
+    let moviesListTitulo = document.querySelector(".moviesListTitulo");
+    let img = document.querySelector("img");
+    let agregar = document.querySelector(".botonAgregar");
 
-    let modo = confirm('Desea modo oscuro');
-    if(modo){
-        body.style.backgroundColor = '#7f7f7f'
-        body.classList.add('fondoMoviesList');
-    }
-    
+    /*  let modo = confirm('Desea modo oscuro');
+  if(modo){
+      body.style.backgroundColor = '#7f7f7f'
+      body.classList.add('fondoMoviesList');
+  } */
+    img.addEventListener("mouseover", () => {
+          body.style.backgroundColor = "#7f7f7f";
+          body.classList.add("fondoMoviesList");
+    });
     console.log(body);
-    moviesListTitulo.innerHTML = 'LISTADO DE PELÍCULAS';
-    moviesListTitulo.style.color ='white';
-    moviesListTitulo.style.backgroundColor = 'teal';
-    moviesListTitulo.style.padding = '20px';
+    moviesListTitulo.innerHTML = "LISTADO DE PELÍCULAS";
+    moviesListTitulo.style.color = "white";
+    moviesListTitulo.style.backgroundColor = "teal";
+    moviesListTitulo.style.padding = "20px";
 
-}
+    agregar.addEventListener("mouseover", () => {
+          agregar.style.color = "yellow";
+          agregar.style.backgroundColor = "teal";
+    });
+    agregar.addEventListener("mouseout", () => {
+        agregar.style.color = "white";
+        agregar.style.backgroundColor = "green";
+  });
+};
